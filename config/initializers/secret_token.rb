708b6e6d82f758984c3629f8cedadc111400819f
dgerbs <dojoh@outlook.com>
2013-12-11 13:05:29 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whaddy::Application.config.secret_key_base = '8926a7f15fbcd4f91ada3cb2401e1b69d8cfbae59e620c2a57d3fba11b47efbdff540ef4c4a7660a2c1d46a5fd44658ce5ee1a7402f97db17025da1ab53449f4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Whaddy::Application.config.session_store :cookie_store, key: '_whaddy_session'
