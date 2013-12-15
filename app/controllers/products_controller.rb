398f2857d5f35b6823a859fc45f6d8a3c1c0f245
dgerbs <dojoh@outlook.com>
2013-12-15 14:24:04 -0500

    authenticate products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only [:edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   def index
     @products = Product.all
@@ -9,14 +11,14 @@ class ProductsController < ApplicationController
   end
 
   def new
-    @product = Product.new
+    @product = current_user.products.build
   end
 
   def edit
   end
 
    def create
-    @product = Product.new(product_params)
+    @product = current_user.products.build(product_params)
     if @product.save
       redirect_to @product, notice: 'Product was successfully created.'
     else
@@ -41,6 +43,10 @@ class ProductsController < ApplicationController
     def set_product
       @product = Product.find(params[:id])
     end
+
+    def correct_user
+      @product = current_user.products.find_by(id: params[:id])
+      redirect_to products_path, notice: "Not authorized to edit this product" if @product.nil?
 
     def product_params
       params.require(:product).permit(:description, :price, :sku, :brand, :condition, :weight, :availability, :shipping, :stock, :quantity)
